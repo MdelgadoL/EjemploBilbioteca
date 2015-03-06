a9a29554c5b86ed8344e6d7a74c28909c3fc8a28
snake_gt <snake_gt@10.60.134.142>
2015-03-06 09:11:22 -0600

    Se agregan 60 nuevos iconos para los menus

---
+++ ProyectoBibliotecaDSW2-ejb/src/java/mx/edu/itschapala/sistemas/biblioteca/dao/AutorLibroDaoLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.edu.itschapala.sistemas.biblioteca.dao;
+
+import java.util.List;
+import javax.ejb.Local;
+import mx.edu.itschapala.sistemas.biblioteca.modelo.AutorLibro;
+
+/**
+ *
+ * @author snake_gt
+ */
+@Local
+public interface AutorLibroDaoLocal {
+
+    void crear(AutorLibro autorLibro);
+
+    void editar(AutorLibro autorLibro);
+
+    void remover(AutorLibro autorLibro);
+
+    AutorLibro buscarPorId(Object id);
+
+    List<AutorLibro> buscarTodos();
+
+    List<AutorLibro> buscarEnRango(int[] range);
+
+    int contar();
+    
+}
